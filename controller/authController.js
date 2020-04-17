8dd8672d3d3d32aced117bd899a963e3b946425a
miracle <anajemiracle@gmail.com>
2020-04-17 01:41:52 +0100

    modified

--- controller/authController.js
+++ controller/authController.js
@@ -1,9 +1,10 @@
 module.exports = {
   isAuthenticated: (req, res, next) => {
-    if (!req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
       req.flash("danger", "Please Login to view this page");
       res.redirect("/login");
     }
-    return next();
   },
 };
